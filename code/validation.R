004188eb2b558a648b7f8fc55a279e88e536da61
John Paige <paigejo@gmail.com>
2023-12-13 16:05:06 +0100

    fixed typo

--- code/validation.R
+++ code/validation.R
@@ -2099,11 +2099,7 @@ getValidationFit = function(fold,
     if(!varClust) {
       parI = match(c("log_tau", "logit_phi", "log_tauEps"), names(testObj$env$last.par))
     } else {
-      if(model %in% c("Md", "MD")) {
-        parI = match(c("log_tau", "logit_phi", "log_tauEpsUrb", "log_tauEpsRur"), names(testObj$env$last.par))
-      } else {
-        parI = match(c("log_tau", "logit_phi", "log_tauEpsUMICS", "log_tauEpsRMICS", "log_tauEpsUDHS", "log_tauEpsRDHS"), names(testObj$env$last.par))
-      }
+      parI = match(c("log_tau", "logit_phi", "log_tauEpsUrb", "log_tauEpsRur"), names(testObj$env$last.par))
     }
     
     if(!all(testObj$env$last.par[parI] == optParStart)) {
